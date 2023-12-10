40d3b1dcb700e5364d20ddbca26497b5eb4a3c35
Alejandro Vidal <alejandro.maximo.vidal@gmail.com>
2023-12-10 18:34:55 -0500

    Added the user input for detereming where to get the card components and where to output the images

--- cardFactory.py
+++ cardFactory.py
@@ -5,9 +5,12 @@ def load_variations(folder):
     files = os.listdir(folder)
     images = [Image.open(os.path.join(folder, file)) for file in files]
     return images
-
-baseImage = Image.open('assets/Bronzeborders/v1 Bronze Border.png')  #Change to file path containing the base 
-background_variations = load_variations('assets/Bronzebackgrounds')  #Change to folder path containing backgrounds
+baseImageInput = input("Enter the file path for the base image: ")
+backgroundInput = input("Enter the path for the background image folder: ")
+playerInput = input("Enter the path for the player image folder: ")
+outputInput = input("Where do you want the images to output: ")
+baseImage = Image.open(baseImageInput)  #Change to file path containing the base 
+background_variations = load_variations(backgroundInput)  #Change to folder path containing backgrounds
 #border_variations = load_variations('assets/mouths') ---- There is only one boarder
 player_variations = load_variations('assets/Bronzeplayers/v1-v2 Bronze-Silver Players') #Change to folder path containing Players
 #def generate_combinations(base, backgrounds, borders, players): ---- orginal code Which acounts for mulitple borders
@@ -23,7 +26,7 @@ player_variations = load_variations('assets/Bronzeplayers/v1-v2 Bronze-Silver Pl
                 #count += 1
 
 def generate_combinations(base, backgrounds, players):
-    output_dir = 'output'
+    output_dir = outputInput
     if not os.path.exists(output_dir):
         os.makedirs(output_dir)
     count = 0
@@ -32,7 +35,7 @@ def generate_combinations(base, backgrounds, players):
                 new_image = base.copy()
                 new_image.paste(background, (0, 0), background)
                 new_image.paste(player, (0, 0), player)
-                new_image.save(f'output/image_{count}.png')
+                new_image.save(f'{output_dir}/image_{count}.png')
                 count += 1
 
 generate_combinations(baseImage, background_variations, player_variations)
