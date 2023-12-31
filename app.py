f5305d4c7bc0a6268310282d18b59d60023ee82a
Alejandro Vidal <alejandro.maximo.vidal@gmail.com>
2023-12-31 14:28:22 -0500

    added a function to delete old files from the server

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask, request, jsonify, render_template, session
 from werkzeug.utils import secure_filename
+from apscheduler.schedulers.background import BackgroundScheduler
 import os
+import time
 from image_merger.merge_images import process_merge
 import uuid
 
@@ -59,6 +61,30 @@ def merge_images():
         return jsonify({"message": "Images merged successfully"}), 200
 
     return jsonify({"error": "Invalid file type"}), 400
+
+#Function to clean up the old files being stored on the server
+def cleanup_old_files():
+    now = time.time()
+    upload_folder = app.config['UPLOAD_FOLDER']
+    max_age = 3600  # Max age for a file in seconds (1 hour in this example)
+
+    for uid_folder in os.listdir(upload_folder):
+        folder_path = os.path.join(upload_folder, uid_folder)
+        if os.path.isdir(folder_path):
+            # Check if the folder is older than the max_age
+            if now - os.path.getmtime(folder_path) > max_age:
+                # Delete the folder
+                for root, dirs, files in os.walk(folder_path, topdown=False):
+                    for name in files:
+                        os.remove(os.path.join(root, name))
+                    for name in dirs:
+                        os.rmdir(os.path.join(root, name))
+                os.rmdir(folder_path)
+                print(f"Deleted old folder: {folder_path}")
+
+scheduler = BackgroundScheduler()
+scheduler.add_job(func=cleanup_old_files, trigger="interval", hours=1)
+scheduler.start()
 
 if __name__ == '__main__':
     app.run(debug=True)
