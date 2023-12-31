d3cd2c973d59868f8b47ed575a93fe429186c8a1
Alejandro Vidal <alejandro.maximo.vidal@gmail.com>
2023-12-31 13:32:35 -0500

    Add session for file mangement

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
-from flask import Flask, request, jsonify, render_template
+from flask import Flask, request, jsonify, render_template, session
 from werkzeug.utils import secure_filename
 import os
 from image_merger.merge_images import process_merge
+import uuid
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'your_secret_key' #Chat Gpt says add secret key idk why 
 UPLOAD_FOLDER = 'uploads'
 ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'bmp', 'gif'}
 
@@ -22,6 +24,11 @@ def allowed_file(filename):
 def merge_images():
     if 'layer1' not in request.files or 'layer2' not in request.files:
         return jsonify({"error": "No file part"}), 400
+    
+    # Ensure the session has a unique ID
+    if 'uid' not in session:
+        session['uid'] = str(uuid.uuid4())
+
 
     file1 = request.files['layer1']
     file2 = request.files['layer2']
@@ -33,14 +40,18 @@ def merge_images():
         filename1 = secure_filename(file1.filename)
         filename2 = secure_filename(file2.filename)
 
-        file1_path = os.path.join(app.config['UPLOAD_FOLDER'], filename1)
-        file2_path = os.path.join(app.config['UPLOAD_FOLDER'], filename2)
+        uid_path = os.path.join(app.config['UPLOAD_FOLDER'], session['uid'])
+        if not os.path.exists(uid_path):
+            os.makedirs(uid_path)
+
+        file1_path = os.path.join(uid_path, filename1)
+        file2_path = os.path.join(uid_path, filename2)
 
         file1.save(file1_path)
         file2.save(file2_path)
 
         
-        output_dir = os.path.join(app.config['UPLOAD_FOLDER'], 'merged')
+        output_dir = os.path.join(uid_path, 'merged')
         if not os.path.exists(output_dir):
             os.makedirs(output_dir)
 
